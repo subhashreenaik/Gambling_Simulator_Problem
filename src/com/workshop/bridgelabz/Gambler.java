c6fc7b3ff3bf52987ab164d86eabbb5ce39ee718
Subhashree naik <subhashree.naik@gmail.com>
2022-02-19 08:34:48 +0530

    Added: initialization

--- src/com/workshop/bridgelabz/Gambler.java
+++ src/com/workshop/bridgelabz/Gambler.java
@@ -1,10 +1,14 @@
 package com.workshop.bridgelabz;
 
 public class Gambler {
-
+	
+	//Declare Constant
+    public final int STAKE=100;
+    public final int BET_FOREACHGAME=1;
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+     
 	}
 
 }
