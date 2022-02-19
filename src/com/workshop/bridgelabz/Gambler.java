36d01a69c626d044565d9ae77a1ea364503ba0d7
Subhashree naik <subhashree.naik@gmail.com>
2022-02-20 01:01:22 +0530

    Added:Lukiest and unlukiest dayof the month program

--- src/com/workshop/bridgelabz/Gambler.java
+++ src/com/workshop/bridgelabz/Gambler.java
@@ -14,6 +14,9 @@ public class Gambler {
     static int min_perStake;
     static int woncount=0;
     static int lostcount=0;
+    static int arr[] = new int[30];
+    static int max=0;
+    static int min=0;
     
     
     
@@ -48,18 +51,36 @@ public class Gambler {
      	if(stake == max_perStake) {
      		winorlost_amnt += max_perStake-fixed_stake;
             System.out.println("Gambler won the game  and resigned for the day "+winorlost_amnt);
-            woncount++;
+            woncount++;//Here it counts total no of days gambler won
             
         }
         if(stake == min_perStake) {
         	winorlost_amnt -= fixed_stake-min_perStake;
         	System.out.println("Gambler lost the game  and resigned for the day "+winorlost_amnt );
-        	lostcount++;
+        	lostcount++; //Here it counts total no of days gambler lost
      	}
+        
+        //stores all win or lost value to array
+        arr[day] = winorlost_amnt;
      	
      	}
+     	
      	System.out.println("Total no of days the gambler won is :"+ woncount);
      	System.out.println("Total no of days the gambler lost is :"+ lostcount);
+     	
+     	
+     	//Here it compute the maximum profit or loss earned in a day
+     	for(int i=0;i<arr.length;i++) {
+			if(arr[i]>max) {
+				max = arr[i];
+			}
+			else if(arr[i]<min) {
+				min=arr[i];
+			}
+			
+		}
+     	System.out.println("The lukiest day where i got maximum "+max);
+     	System.out.println("The unlukiest day where i lost maximum "+min);
      	
     }
     
