1c7497afdb35d59225929c55c6e8966a5c333c58
Subhashree naik <subhashree.naik@gmail.com>
2022-02-19 09:44:58 +0530

    Added: Resignoftheday Program

--- src/com/workshop/bridgelabz/Gambler.java
+++ src/com/workshop/bridgelabz/Gambler.java
@@ -8,20 +8,39 @@ public class Gambler {
     
     //Declare variables
     static int bet=1;
+    static int stake=100;
+    
     
     static void checkingWinOrLoose() {
-     	 double check=(int)(Math.random() *10)%2; 
-     	if(check == 1 )
+     	
+     	while(stake < 150 && stake > 50 ) {
+     	int check=(int)(Math.random() *10)%2; 
+     	switch(check){
+     	
+     	case 1:
         {
-       	 bet++;
-           System.out.println("Gamler is winning "+bet); 	
+       	 stake+=bet;
+       	 break;
+           //System.out.println("Gamler is winning "+stake); 	
         }
-        else
+        default:
         {
-          bet--;
-        	System.out.println("Gambler is loosing  "+bet);
+          stake -=bet;
+          break;
+        	
          }
       }
+     	
+     	
+    }
+     	if(stake ==150) {
+     	System.out.println("Gambler won the game  and resigned for the day ");
+    }
+    if(stake == 50) {
+    	System.out.println("Gambler lost the game  and resigned for the day ");
+    }
+    }
+     	
    
     
 	public static void main(String[] args) {
