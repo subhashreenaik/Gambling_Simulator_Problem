c305d30590b41b0101148079d15c346bab2c9481
Subhashree naik <subhashree.naik@gmail.com>
2022-02-19 08:55:33 +0530

    Added: checkingWinOrLoosing

--- src/com/workshop/bridgelabz/Gambler.java
+++ src/com/workshop/bridgelabz/Gambler.java
@@ -6,9 +6,27 @@ public class Gambler {
     public final int STAKE=100;
     public final int BET_FOREACHGAME=1;
     
+    //Declare variables
+    static int bet=1;
+    
+    static void checkingWinOrLoose() {
+     	 double check=(int)(Math.random() *10)%2; 
+     	if(check == 1 )
+        {
+       	 bet++;
+           System.out.println("Gamler is winning "+bet); 	
+        }
+        else
+        {
+          bet--;
+        	System.out.println("Gambler is loosing  "+bet);
+         }
+      }
+   
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     
+		checkingWinOrLoose();
 	}
 
 }
